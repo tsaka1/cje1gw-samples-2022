bc8964bab5f588c2badd363192e09a69f49cc1dd
Tetsuo Sakaguchi <saka@slis.tsukuba.ac.jp>
2023-06-05 10:25:33 +0900

    change server to wsgiref.
    Flaskの古いバージョンが依存するパッケージの関係で使うのが難しくなったので、それに対応しました。
    そのために、wsgirefのテストサーバを使うように変更しました。シングルスレッドサーバになります。
    最新のFlaskで使用することが出来ます。

--- pycode.cje1gw.__init__.py
+++ pycode.cje1gw.__init__.py
@@ -1,19 +1,20 @@
 #
 # cje1gw - a gateway like reverse-proxy inspired by ngrok service
 #	by saka@slis.tsukuba.ac.jp
-#       $Id: __init__.py,v 1.4 2022/06/10 09:55:41 saka Exp $
+#       $Id: __init__.py,v 2.0 2023/06/05 01:03:13 saka Exp $
 #
 
 from subprocess import Popen, PIPE
 import atexit
 import time
+from wsgiref.simple_server import make_server, WSGIServer
 
 user_host = "example@example.com"
 id_rsa = "/root/.ssh/id_rsa-cje1"
 
 def _test():
     print("This is a test.")
-    print("$Id: __init__.py,v 1.4 2022/06/10 09:55:41 saka Exp $")
+    print("$Id: __init__.py,v 2.0 2023/06/05 01:03:13 saka Exp $")
 
 def _get_port_and_url():
     with Popen(["ssh", "-i", id_rsa, user_host, "port"], stdout=PIPE) as sp:
@@ -35,10 +36,18 @@ def run_with(app):
 
     def new_run(*args, **kwargs):
         port = kwargs.get('port', 5000)
+        host = kwargs.get('host', '127.0.0.1')
         src_port, url = _get_port_and_url()
         _run_gateway(src_port, port)
         time.sleep(1)
         print(f" * Running on {url}")
-        old_run(*args, **kwargs)
+        WSGIServer.allow_reuse_address = True
+        server = make_server(host, port, app)
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            print("Detect KeyboardInterrupt. Stop.")
+        finally:
+            server.server_close()
     app.run = new_run
 
